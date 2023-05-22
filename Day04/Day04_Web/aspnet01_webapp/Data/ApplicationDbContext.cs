eb5b8375b53f1f6b4a91ec170d939be70f2638e2
YongHwanLee97 <dldyd1101@gmail.com>
2023-05-22 17:29:32 +0900

    5.22

---
+++ Day04/Day04_Web/aspnet01_webapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using aspnet01_webapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet01_webapp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Board> Boards { get; set; }
+    }
+}
